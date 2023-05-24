33c60e2a755c1e9a11d4b1a6fb76d7750c3b0777
example <96044353+nisanurbastor@users.noreply.github.com>
2023-05-24 17:13:20 +0300

    JUnit5

---
+++ JUnit_Homework/src/main/java/org/example/Calculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Calculator {
+    public static double divide(double a, double b) {
+        double result;
+        if (b == 0) throw new IllegalArgumentException("Divide by zero exception.");
+        result = a / b;
+        return result;
+    }
+}
